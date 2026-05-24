5d2a9c698b0807f5093619c70a92b227037e4c29
HERVICAS <152441794+HERVICAS@users.noreply.github.com>
2026-05-24 13:55:28 -0500

    Update conc.py

--- conc.py
+++ conc.py
@@ -51,16 +51,17 @@ st.title("🧠 CONCÉNTRECE HVC")
 st.markdown("""
     <style>
     /* Buscamos todos los botones dentro del tablero y les damos tamaño */
-    .tablero-grande div.stButton > button {
+    div.stButton > button {
         width: 160px !important;       
         height: 120px !important;      
-        border-radius: 12px !important;
+        border-radius: 12px !important; 
     }
     
     /* 2. NUEVO: Fuerza el tamaño gigante para los números y los emojis internos */
-    .tablero-grande div.stButton > button p {
-        font-size: 50px !important;    
+    div.stButton > button p {
+        font-size: 50px !important;    /* Tamaño ideal para que los emojis queden proporcionales */
         font-weight: bold !important;
+        line-height: 120px !important; /* Centra el emoji perfectamente a lo alto del botón */
     }
     </style>
 """, unsafe_allow_html=True)
